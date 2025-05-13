0446924b178f2dfaf8f85040767917899f38a2e5
Antoine Hazebrouck <antoine.hazebrouck5@gmail.com>
2025-05-13 13:53:33 +0200

    works on xiaomi

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,7 +11,8 @@ plugins {
 android {
     namespace = "com.example.chti_face_bouc"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    // ndkVersion = flutter.ndkVersion
+	ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -27,10 +28,12 @@ android {
         applicationId = "com.example.chti_face_bouc"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        // minSdk = flutter.minSdkVersion
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+		// minSdkVersion = 23
     }
 
     buildTypes {
